f054583c632288f1ccbe62358a16bc8765fe29d5
Marwan Al Jubeh <jubeh@google.com>
2018-02-06 16:02:28 +0000

    Fix the copyright year in tests/oneof_fields.go

--- tests/oneof_fields.go
+++ tests/oneof_fields.go
@@ -1,4 +1,4 @@
-// Copyright 2017 The ObjectHash-Proto Authors
+// Copyright 2018 The ObjectHash-Proto Authors
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
